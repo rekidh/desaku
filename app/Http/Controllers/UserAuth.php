c41f7e7d865e1f1a3a019412728575ba00668ebd
rekidh <89791981+rekidh@users.noreply.github.com>
2022-10-29 22:22:00 +0700

    line terhapus

--- app/Http/Controllers/UserAuth.php
+++ app/Http/Controllers/UserAuth.php
@@ -120,7 +120,7 @@ class UserAuth extends Controller
             // named
             $name = str_replace(' ', '_', Auth::user()->name);
             $imageName = $name . '_' . $h["year"] . '_' . $h["mon"] . '_' . $h["mday"] . 'S' . $h[0] .  '.' . $image_type; // name format
-            Storage::makeDirectory('image/');
+            Storage::makeDirectory('image');
             $req->image->move(public_path('images/'), $imageName);
             $folderPath = public_path('image/');
             $imageFullPath = $folderPath . $imageName;
